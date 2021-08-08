b61b4d88d03e822876c24aabd762f523ed3c7fe7
Vadim Rufov <vadim.rufov@gs-labs.ru>
2021-08-08 10:08:31 +0300

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Вадим Руфов on 08.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
